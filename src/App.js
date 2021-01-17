f0f55d10ea23764f74096e7d5ee1dd6cb197e4f4
Pavel-Tymoshchuk <38438426+Pavel-Tymoshchuk@users.noreply.github.com>
2021-01-17 17:19:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [boards, setBoards] = useState([
+        {id: 1, title: 'Сделать', items: [{id: 1, text: "Купить хлеб"},{id: 2, text: "Выбросить мусор"},{id: 3, text: "Как сделать create-react-app"}]},
+        {id: 2, title: 'В работе', items: [{id: 1, text: "Реализация приложения"},{id: 2, text: "Покупка телефона"}]},
+        {id: 3, title: 'Готово', items: [{id: 1, text: "Выучить основы React"},{id: 2, text: "Сделать приложание Drag and drop используюя React"}]},
+    ]);
+    
+    const [currentBoard, setCurrentBord] = useState(null);
+    const [currentItem, setCurrentItem] = useState(null)
+    
+    function dragOverHendler(e) {
+        e.preventDefault();
+        if(e.target.className === 'drag-elem') {
+            e.target.style.boxShadow = "0px 2px 3px gray";
+        }
+    }
+    function dragLeaveHendler(e) {
+        e.target.style.boxShadow = "none";
+        
+    }
+    function dragStartHendler(board,item) {
+        setCurrentBord(board);
+        setCurrentItem(item);
+    }
+    function dragEndHendler(e) {
+        e.target.style.boxShadow = "none";
+    }
+    function dropHendler(e,board,item) {
+        e.preventDefault();
+        const currentIndex = currentBoard.items.indexOf(currentItem);
+        currentBoard.items.splice(currentIndex,1);
+        const dropIndex = board.items.indexOf(item);
+        board.items.splice(dropIndex + 1,0, currentItem);
+        setBoards(boards.map(b => {
+            if(b.id === board.id) {
+                return board;
+            }
+            if(b.id === currentBoard.id) {
+                return currentBoard;
+            }
+            
+            return b;
+        }));
+        e.target.style.boxShadow = "none";
+    }
+    
+    function dropCardHendler(e,board) {
+        e.preventDefault();
+        if(e.target.className !== "drag-elem") {
+            board.items.push(currentItem);
+            const currentIndex = currentBoard.items.indexOf(currentItem);
+            currentBoard.items.splice(currentIndex,1);
+            
+            setBoards(boards.map(b => {
+                if(b.id === board.id) {
+                    return board;
+                }
+                if(b.id === currentBoard.id) {
+                    return currentBoard;
+                }
+                
+                return b;
+            }));
+        }
+    }
+    
+    return (
+        <ul className="drag-list">
+            {boards.map((board,index) =>{
+                return (
+                <li className="drag-item" 
+                    key={index} 
+                    onDragOver={(e) => dragOverHendler(e)} 
+                    onDrop={(e) => dropCardHendler(e,board)}>
+                    <p className="drag-item__caption">{board.title}</p>
+                    {board.items.map((item,index) => {
+                        return(
+                        <div className="drag-elem" key={index}
+                        draggable={true}
+                        onDragOver={(e) => dragOverHendler(e)}
+                        onDragLeave={e => dragLeaveHendler(e)}
+                        onDragStart={e => dragStartHendler(board,item)}
+                        onDragEnd={e => dragEndHendler(e)}
+                        onDrop={e => dropHendler(e,board,item)}
+                        >
+                            {item.text}</div>
+                    )})}
+                </li>
+            )})}
+        </ul>
+    );
 }
 
 export default App;
